1c517b2523082790473ccd37521dfaa2a0db3931
Thorben Römer <thorbenroemer@t-online.de>
2020-07-12 18:06:32 +0200

    add support for monthly effects, add debug mode
    Signed-off-by: Thorben Römer <thorbenroemer@t-online.de>

--- bot.py
+++ bot.py
@@ -39,22 +39,36 @@ def get_weekly_activities():
 
     return activities
 
+def get_monthly_activities():
+    html = requests.get("https://wiki.guildwars.com/wiki/Flux").text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    tr = soup.find('tr', attrs = {'style' : "font-weight:bold"})
+
+    activities = {}
+
+    activities["flux"] = { "type": "Flux", "name" : tr.contents[3].find("a").string }
+
+    return activities
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Guild Wars Zaishen Mission Notifier")
     parser.add_argument("-d", "--daily", action="store_true", help="check daily missions", dest="daily")
-    parser.add_argument("-w", "--weekly", action="store_true", help="check weekly missions", dest="weekly")
+    parser.add_argument("-w", "--weekly", action="store_true", help="check weekly bonuses", dest="weekly")
+    parser.add_argument("-m", "--monthly", action="store_true", help="check monthly effects", dest="monthly")
     parser.add_argument("-a", "--alert", action="store_true", help="announce matched missions via telegram", dest="alert")
     parser.add_argument("-p", "--post", action="store_true", help="post all missions via telegram", dest="post")
+    parser.add_argument("--debug", action="store_true", help="don't send via telegram, print to stdout instead", dest="debug")
 
     args = parser.parse_args()
 
-    if not (args.daily or args.weekly):
-        print("specify either --daily or --weekly")
+    if not (args.daily or args.weekly or args.monthly):
+        print("specify at least one of --daily, --weekly or --monthly")
         parser.print_help()
         exit(1)
 
     if not (args.alert or args.post):
-        print("specify either --alert or --post")
+        print("specify at least one of --alert or --post")
         parser.print_help()
         exit(1)
 
@@ -74,12 +88,13 @@ if __name__ == "__main__":
         if args.alert:
             matches = ""
 
-            for mission_type in dailies.keys():
-                if dailies[mission_type]["name"] in config["daily"][mission_type]:
-                    matches += "{}: {}\n".format(dailies[mission_type]["type"], dailies[mission_type]["name"])
+            if "daily" in config.keys():
+                for mission_type in dailies.keys():
+                    if dailies[mission_type]["name"] in config["daily"][mission_type]:
+                        matches += "{}: {}\n".format(dailies[mission_type]["type"], dailies[mission_type]["name"])
 
             if matches != "":
-                matches = "Your matched missions for today:\n" + matches
+                matches = "Your matched missions for today:\n{}".format(matches)
 
                 if message != "":
                     matches = "\n" + matches
@@ -101,18 +116,50 @@ if __name__ == "__main__":
         if args.alert:
             matches = ""
 
-            for bonus_type in weeklies.keys():
-                if weeklies[bonus_type]["name"] in config["weekly"][bonus_type]:
-                    matches += "{}: {}\n".format(weeklies[bonus_type]["type"], weeklies[bonus_type]["name"])
+            if "weekly" in config.keys():
+                for bonus_type in weeklies.keys():
+                    if weeklies[bonus_type]["name"] in config["weekly"][bonus_type]:
+                        matches += "{}: {}\n".format(weeklies[bonus_type]["type"], weeklies[bonus_type]["name"])
 
             if matches != "":
-                matches = "Your matched bonuses for this week:\n" + matches
+                matches = "Your matched bonuses for this week:\n{}".format(matches)
 
                 if message != "":
                     matches = "\n" + matches
 
-                    message += matches
+                message += matches
+
+    if args.monthly:
+        monthlies = get_monthly_activities()
+
+        if args.post:
+            if message != "":
+                message += "\n"
+
+            message += "This Month's Effects:\n"
+
+            for m in monthlies.values():
+                message += "{}: {}\n".format(m["type"], m["name"])
+
+        if args.alert:
+            matches = ""
+
+            if "monthly" in config.keys():
+                for effect in monthlies.keys():
+                    if monthlies[effect]["name"] in config["monthly"][effect]:
+                        matches += "{}: {}\n".format(monthlies[effect]["type"], monthlies[effect]["name"])
+
+            if matches != "":
+                matches = "Your matched effects for this month:\n{}".format(matches)
+
+                if message != "":
+                    matches = "\n" + matches
+
+                message += matches
 
     if message != "":
-        bot = telebot.TeleBot(config["token"])
-        bot.send_message(config["chat_id"], message)
+        if args.debug:
+            print(message)
+        else:
+            bot = telebot.TeleBot(config["token"])
+            bot.send_message(config["chat_id"], message)
